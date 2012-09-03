f59ceb0661f228ffce947df7461fad6a495ea285
Alexandra Keller <alexandra@eye3ware.com>
2012-09-03 14:01:58 +0200

    decoupling tests from application

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -15,4 +15,12 @@ describe ApplicationHelper do
       full_title("").should_not =~ /\|/
     end
   end
+  
+  RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+  
+  
 end
