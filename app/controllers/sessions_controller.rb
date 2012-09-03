36863807080149e9925c3b5b579203ff74acd2fe
Alexandra Keller <alexandra@eye3ware.com>
2012-09-03 15:38:00 +0200

    changed form for sign up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ def new
   end
 
   def create
-   user = User.find_by_email(params[:session][:email].downcase)
-  if user && user.authenticate(params[:session][:password])
+   user = User.find_by_email(params[:email].downcase)
+  if user && user.authenticate(params[:password])
       sign_in user
       redirect_to user  else
     flash.now[:error] = 'Invalid email/password combination' # Not quite right!
