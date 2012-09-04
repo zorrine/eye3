ea814664e7d7b9f20dcec39d4be5f7b6253c47f7
Alexandra Keller <alexandra@eye3ware.com>
2012-09-04 18:49:29 +0200

    added user index

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,6 +10,26 @@ describe "User pages" do
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
   end
+  describe "index" do
+    before do
+      sign_in FactoryGirl.create(:user)
+      FactoryGirl.create(:user, name: "Bob", email: "bobs@example.com")
+      FactoryGirl.create(:user, name: "Ben", email: "bens@example.com")
+      visit users_path
+    end
+
+    it { should have_selector('title', text: 'All users') }
+    it { should have_selector('h1',    text: 'All users') }
+
+    it "should list each user" do
+      User.all.each do |user|
+        page.should have_selector('li', text: user.name)
+      end
+    end
+  end
+  
+  
+ 
   
   
   
