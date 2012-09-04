2477b0a1790b34a0bec9119eabc384584ef2843a
Alexandra Keller <alexandra@eye3ware.com>
2012-09-04 10:02:25 +0200

    chapter 9, authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-  
+    before_filter :signed_in_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
+  
   def create
     @user = User.new(params[:user])
     if @user.save
@@ -13,8 +14,30 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
   def new
     @user = User.new
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+private
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
   
 end
