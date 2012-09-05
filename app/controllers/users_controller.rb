502af8acf9cb3120e9311ea3bbc8db69cf672dea
Alexandra Keller <alexandra@eye3ware.com>
2012-09-05 11:04:07 +0200

    Finished user edit,update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-    before_filter :signed_in_user, only: [:index, :edit, :update]
+    before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
      before_filter :correct_user,   only: [:edit, :update]
+      before_filter :admin_user,     only: :destroy
   def show
     @user = User.find(params[:id])
   end
@@ -34,9 +35,18 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
   def index
    @users = User.paginate(page: params[:page])
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+  
+  
 private
 
     def signed_in_user
@@ -49,6 +59,10 @@ private
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
   
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,12 +4,15 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
     self.current_user = user
   end
+  
   def signed_in?
     !current_user.nil?
   end
+  
   def current_user=(user)
     @current_user = user
   end
+  
    def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
@@ -21,6 +24,7 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end
+  
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
