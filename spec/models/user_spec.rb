7897d1ff948226c9e4c2fae6ac4b55c6d979197c
Alexandra Keller <alexandra@eye3ware.com>
2012-09-10 10:40:24 +0200

    added all tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -40,6 +40,8 @@ describe User do
   it { should respond_to(:following?) }
   it { should respond_to(:follow!) }
   it { should respond_to(:unfollow!) }
+   it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
   
    it { should be_valid }
    it { should_not be_admin }
@@ -192,6 +194,15 @@ end
       it { should_not be_following(other_user) }
       its(:followed_users) { should_not include(other_user) }
     end
+    
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+    
     
   end
   
