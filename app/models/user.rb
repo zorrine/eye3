59ecc44f006157d354e503dfc4bdf577856a14f7
Alexandra Keller <alexandra@eye3ware.com>
2012-09-11 09:23:54 +0200

    add advanced feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,6 +52,9 @@ class User < ActiveRecord::Base
     relationships.find_by_followed_id(other_user.id).destroy
   end
   
+  def feed
+    Micropost.from_users_followed_by(self)
+  end
   
   private
 
